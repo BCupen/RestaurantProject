1ff05983e5b379b2da2bf98368dc9d0bd5acb423
BCupen <bacupen@gmail.com>
2022-11-29 15:42:02 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ function clearContent(){
     while(content.hasChildNodes()){
         content.removeChild(content.firstChild);
     }
+    navButtons.forEach(button =>{
+        if(button.classList.contains('active'))
+            button.classList.remove('active');
+    })
     body.removeChild(body.lastChild);
 }
 
@@ -23,30 +27,38 @@ const menuButton = document.querySelector('.menu-button');
 let homeMenuButton = document.querySelector('.welcome-menu-button');
 const homeButton = document.querySelector('.home-button');
 const contactButton = document.querySelector('.contact-button');
+const nav = document.querySelector('nav');
+const navButtons = nav.childNodes;
+
+homeButton.classList.add('active');
 
 menuButton.addEventListener('click', (e)=>{
-    console.log(e.target);
     clearContent();
+    e.target.classList.add('active');
     loadMenuPage(content);
 });
 
 homeMenuButton.addEventListener('click', (e)=>{
     clearContent();
+    menuButton.classList.add('active')
     loadMenuPage(content);
 });
 
 homeButton.addEventListener('click', (e)=>{
     clearContent();
+    e.target.classList.add('active');
     loadHomePage(content);
     homeMenuButton = document.querySelector('.welcome-menu-button');
     homeMenuButton.addEventListener('click', (e)=>{
         clearContent();
+        menuButton.classList.add('active');
         loadMenuPage(content);
     });
 });
 
 contactButton.addEventListener('click', (e)=>{
     clearContent();
+    e.target.classList.add('active');
     loadContactPage(content);
 })
 
